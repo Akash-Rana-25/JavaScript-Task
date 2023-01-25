f0cd94745ff7c518e972e30d460f96046d049db7
Akash-Rana-25 <akashrana.work.gmail.com>
2023-01-25 17:55:48 +0530

    javaScript added

--- js/test.js
+++ js/test.js
@@ -1,35 +1,25 @@
-// let x=5;
-// let y=8;
-// const z=10;
-// let string="test"
-// let a;
-// let b=typeof a;
+function display(val){
 
-// let x = myFunction(4, 3);   
+    document.getElementById('result').value += val
 
-// function myFunction(a, b) {
-//   return a * b;         
-// }
-// console.log(b);
-// console.log(string +" "+ z);
-// console.log(z);
-// console.log(x);
-// console.log(x<y);
-// console.log(x+y);
-// console.log(x*y);
+    return val
+   
 
-// console.log(x=5||y=10);
+}
 
-// let f="this 'string' testing"
-// let k=f.length;
-// let k=f.slice(5,8)
-// let k=f.substr(4)
-// let k=f.replace("'string'","string")
-// let k=f.split(",")
-// console.log(k);
+function clearScreen(){
 
-// let fn="akash";
-// let ln ="Rana";
+    document.getElementById('result').value = ''
 
-// let name = `${fn} ${ln}`;
-// console.log(name)
+}
+function solve(){
+
+    let x = document.getElementById('result').value
+
+    let y = eval(x);
+
+    document.getElementById('result').value = y
+
+    return y
+
+}
