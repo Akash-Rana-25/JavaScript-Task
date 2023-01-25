dfaa59e0f284ee965a452be3859b656f39b67a90
Akash-Rana-25 <akashrana.work.gmail.com>
2023-01-25 16:28:30 +0530

    task added

---
+++ js/test.js
@@ -0,0 +1,35 @@
+// let x=5;
+// let y=8;
+// const z=10;
+// let string="test"
+// let a;
+// let b=typeof a;
+
+// let x = myFunction(4, 3);   
+
+// function myFunction(a, b) {
+//   return a * b;         
+// }
+// console.log(b);
+// console.log(string +" "+ z);
+// console.log(z);
+// console.log(x);
+// console.log(x<y);
+// console.log(x+y);
+// console.log(x*y);
+
+// console.log(x=5||y=10);
+
+// let f="this 'string' testing"
+// let k=f.length;
+// let k=f.slice(5,8)
+// let k=f.substr(4)
+// let k=f.replace("'string'","string")
+// let k=f.split(",")
+// console.log(k);
+
+// let fn="akash";
+// let ln ="Rana";
+
+// let name = `${fn} ${ln}`;
+// console.log(name)
